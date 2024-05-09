22aff54f4ff4e80ac44cffb35bb80237b876d557
tejaramgurjar <tejaram20199@gmail.com>
2024-05-09 10:04:51 +0530

    add animation  file

---
+++ portfolio.js
@@ -0,0 +1,58 @@
+gsap.from('.for-language .col-xl-2',{
+    y:60,
+    duration:1,
+    opacity:0,
+    stagger:1,
+    scrollTrigger:'.for-language'
+        
+    })
+
+gsap.from('.portfolio-intro',{
+    y:60,
+    duration:1.5,
+    opacity:0,
+    scrollTrigger:'.portfolio-intro'
+        
+    })
+
+gsap.from('.text1',{
+    y:60,
+    duration:1.5,
+    opacity:0,
+    scrollTrigger:'.text1'
+        
+    })
+
+
+gsap.from('.projects1',{
+    y:60,
+    duration:1.5,
+    opacity:0,
+    scrollTrigger:'.projects1'
+        
+    })
+
+gsap.from('.text2',{
+    y:60,
+    duration:1.5,
+    opacity:0,
+    scrollTrigger:'.text2'
+        
+    })
+
+
+gsap.from('.Experience .col-xl-5',{
+    x:60,
+    duration:1.5,
+    opacity:0,
+    scrollTrigger:'.Experience'
+        
+    })
+
+gsap.from('.Experience .col-xl-7',{
+    x:-60,
+    duration:1.5,
+    opacity:0,
+    scrollTrigger:'.Experience'
+        
+    })
